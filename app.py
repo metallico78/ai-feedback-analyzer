f2282b678bc830b791bc6b1ddbc4c047fc4d6865
metallico78 <padillaelectrico@yahoo.com>
2026-02-25 12:16:16 -0500

    Resolver conflictos y limpiar app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+import os
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hola desde ai-feedback-analyzer con FastAPI!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 from fastapi import FastAPI, HTTPException, Depends, Header
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
